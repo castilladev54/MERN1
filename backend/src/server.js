6c90068b36151736f0868133160a253d689dab55
castilladev54 <castilladev54@gmail.com>
2025-06-30 13:49:27 -0400

    prepared for the deployme

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,25 @@ import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
 import dotenv from "dotenv";
+import path from "path";
+
 dotenv.config();
 
 // Initialize express app
 const app = express();
 // Puerto de la aplicación
 const PORT = process.env.PORT || 5001;
+//
+const __dirname=path.resolve();
 
 //middleware
-app.use(
+if(process.env.NODE_ENV !== "production"){
+  app.use(
   cors({
     origin:"http://localhost:5173"
   })
 ); 
+}
 
 // Parse JSON bodies
 app.use(express.json());
@@ -24,6 +30,16 @@ app.use(express.json());
 app.use(rateLimiter);
 // Rutas
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  //
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+//
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+})
+}
 
 // Ruta de inicio con MongoDB
 connectDB().then(() => {
---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//in production, there´s no localhost se we to make this dynamic
+
+const BASE_URL= import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+})
+
+export default api;
+
